71a96631d2ef3561e2e14242be40af11f71cbeb5
Cesar Romagnolo <cesar.romagnolo@dextra-sw.com>
2018-09-19 18:23:11 -0300

    initial Commit

---
+++ src/main/java/animalClass.java
@@ -0,0 +1,38 @@
+public class animalClass {
+    private String nome;
+    private String especie;
+    private String Tipo;
+    private Boolean ativo;
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getEspecie() {
+        return especie;
+    }
+
+    public void setEspecie(String especie) {
+        this.especie = especie;
+    }
+
+    public String getTipo() {
+        return Tipo;
+    }
+
+    public void setTipo(String tipo) {
+        Tipo = tipo;
+    }
+
+    public Boolean getAtivo() {
+        return ativo;
+    }
+
+    public void setAtivo(Boolean ativo) {
+        this.ativo = ativo;
+    }
+}
---
+++ src/main/java/main.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+
+public class main{
+    public static void main(String[] args) {
+        Scanner ler = new Scanner(System.in);
+
+        int n;
+
+        System.out.printf("Informe o número para a tabuada:\n");
+        n = ler.nextInt(); // 3.1 entrada de dados (lendo um valor inteiro)
+        System.out.println(n);
+    }
+}
+
